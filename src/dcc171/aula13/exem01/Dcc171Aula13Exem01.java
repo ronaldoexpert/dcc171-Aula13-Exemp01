947729d459bdfddce3ac2866b635bf777619c517
Ronaldo Silveira <contato@expertme.com.br>
2017-11-14 21:57:52 -0200

    Criação da inserção de produto e listar produto

--- src/dcc171/aula13/exem01/Dcc171Aula13Exem01.java
+++ src/dcc171/aula13/exem01/Dcc171Aula13Exem01.java
@@ -2,6 +2,8 @@ package dcc171.aula13.exem01;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -14,8 +16,29 @@ public class Dcc171Aula13Exem01 {
         try {
             Connection conexao = DriverManager.getConnection(driverUrl, "usuario", "senha");
             //teste de alteração
+            PreparedStatement operacaoInsere = conexao.prepareCall("insert into PRODUTO (nome, qtd, atualizado)"
+                    + " values(?,?, current_timestamp)");
+            operacaoInsere.clearParameters();
+            operacaoInsere.setString(1, "Bala55");
+            operacaoInsere.setInt(2, 102);
+            operacaoInsere.executeUpdate();
             
-            //Teste alteração Lucas
+            PreparedStatement operacaoListar = conexao.prepareCall(""
+                    + "Select nome, qtd from produto where qtd > ?");
+            operacaoListar.clearParameters();
+            operacaoListar.setInt(1, 100);
+            ResultSet resultado = operacaoListar.executeQuery();
+            
+            while (resultado.next()) {
+                System.out.println(
+                        resultado.getString(1) + 
+                        "\t\t" + 
+                        resultado.getInt(2)
+                        );                
+            }
+            
+                
+            
         } catch (SQLException ex) {
             Logger.getLogger(Dcc171Aula13Exem01.class.getName()).log(Level.SEVERE, null, ex);
         }
